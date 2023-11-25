1c636a89e5bb49dfb16b68ba600169c7d12c1b1d
yongseongkim <yongseongkimm@gmail.com>
2023-11-25 14:45:38 +0900

    Spoqa 폰트 추가하기

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+export type Font = SpoqaHanSans;
+
+export enum SpoqaHanSans {
+  Bold = 'SpoqaHanSansNeo-Bold', // 700
+  Medium = 'SpoqaHanSansNeo-Medium', // 500
+  Regular = 'SpoqaHanSansNeo-Regular', // 400
+  Light = 'SpoqaHanSansNeo-Light', // 300
+  Thin = 'SpoqaHanSansNeo-Thin', // 100
+}
